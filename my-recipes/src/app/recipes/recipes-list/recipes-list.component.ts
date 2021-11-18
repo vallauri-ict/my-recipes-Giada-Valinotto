9c23b5dd1a7ec73af60ddbd202f9741f55dd5b04
Giada Valinotto <g.valinotto.0990@vallauri.edu>
2021-11-18 10:14:16 +0100

    Model creation

--- my-recipes/src/app/recipes/recipes-list/recipes-list.component.ts
+++ my-recipes/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RecipeModel } from '../../models/recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -7,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipesListComponent implements OnInit {
 
+  recipes:RecipeModel[]=[
+    
+  ]
   constructor() { }
 
   ngOnInit(): void {
