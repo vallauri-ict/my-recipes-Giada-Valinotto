33228f9b70a5fea496f051c210e22ab2ef51924d
Giada Valinotto <g.valinotto.0990@vallauri.edu>
2021-12-10 12:55:12 +0100

    Angular RecipeBook: View the details of the selected recipe

--- my-recipes/src/app/recipes/recipe-list/recipe-list.component.ts
+++ my-recipes/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { RecipeModel } from 'src/app/models/recipe.models';
 
 @Component({
@@ -7,35 +7,43 @@ import { RecipeModel } from 'src/app/models/recipe.models';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes :RecipeModel[]=[
-    new RecipeModel(
-      "Spaghetti alla chitarra",
-      "Un particolare tipo di pasta che...",
-      "https://images.lacucinaitaliana.it/wp-content/uploads/2018/08/01223240/Spaghetti-alla-chitarra-con-pallottine-1600x800.jpg"
-    ),
-    new RecipeModel(
-      "Lasagne alla bolognese",
-      "Che buone le lasagne",
-      "https://www.tribugolosa.com/uploads/media/lasagne-alla-bolognese-hem1.jpg?1389133420"
-    ),
-    new RecipeModel(
-      "Gnocchi ai formaggi",
-      "Ottimi soprattutto in Val Varaita, nella variante Ravioles",
-      "https://www.piemontetopnews.it/wp-content/uploads/2020/02/Ravioles-2.jpg"
-    ),
-    new RecipeModel(
-    "Tiramisù",
-    "Classico dolce italiano con caffè e mascarpone",
-    "https://www.milanocittastato.it/wp-content/uploads/2020/08/la-ricetta-classica-del-tiramisu-con-uova-savoiardi-e-mascarpone-1920x1080-1.jpg")
-  ]
+  @Output() recipeSelectedInList = new EventEmitter<RecipeModel>()
 
+  recipes:RecipeModel[]=[
+    new RecipeModel(
+      'Spaghetti alla chitarra',
+      'Un particolare tipo di pasta che ...',
+      'https://images.lacucinaitaliana.it/wp-content/uploads/2020/06/03204258/Pasta-alla-chitarra-con-ragu-di-polo.jpg'
+    ),
+
+    new RecipeModel(
+      'Lasagne alla bolognese',
+      'Pasta emiliana molto calorica sempre presente nei pranzi domenicali ...',
+      'https://www.tribugolosa.com/uploads/media/lasagne-alla-bolognese-hem1.jpg?1389133420'
+    ),
+
+    new RecipeModel(
+      'Ravioles al Formaggio',
+      'Ottimi soprattutto in Val Varaita nella variante Ravioles...',
+      'https://media-cdn.tripadvisor.com/media/photo-s/14/b1/37/d3/ravioles-della-valle.jpg'
+    ),
+
+    new RecipeModel(
+      'Tiramisù',
+      'Classico dolce italiano con mascarpone ...',
+      'https://toscanedigusto.com/wp-content/uploads/2019/09/maxresdefault.jpg'
+    )
+  ]
 
   selectedRecipe:RecipeModel
   constructor() {
     this.selectedRecipe=this.recipes[0]
    }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onRecipeSelected(recipe:RecipeModel){
+    this.recipeSelectedInList.emit(recipe);
   }
 
 }
